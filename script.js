6a79e339037209b5e1f7ea10c1644c2c313d11fa
mattgiordano97 <mattgiordano97@gmail.com>
2024-10-16 20:17:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('btn');
+const inputText = document.getElementById('codice-segreto');
+const result = document.getElementById("result");
+const hiddenLink = document.getElementById("hidden-link");
+
+const insulti = ["e sbagliat", "par a fess", 'ma lo hai scritto bene?', 'boh io basito'];
+let insultiIndex = 0;
+
+button.addEventListener("click", () => {
+    console.log('click');
+    const input = inputText.value;
+    if (typeof input !== 'string') {
+        return;
+    }
+    const password = input.toLowerCase();
+
+    if (password === 'trombetti') {
+        console.log("codice corretto");
+        result.textContent = "Sempre sia lodato. Ora clicca il coso blu.";
+        hiddenLink.hidden = false;
+    }
+    else {
+        result.textContent = insulti[insultiIndex];
+        insultiIndex = (insultiIndex + 1) % insulti.length;
+    }
+});
